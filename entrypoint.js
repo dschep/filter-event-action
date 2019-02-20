fe0ae0d90a7cf4562ef1a42b77840e531cdf064d
Daniel Schep <dschep@gmail.com>
2019-02-19 19:20:58 -0500

    oops, negate the if statment for exit 78

--- entrypoint.js
+++ entrypoint.js
@@ -5,7 +5,7 @@ const vm = require('vm')
 
 const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
 
-if (vm.runInNewContext(process.argv[2], { event }))
+if (!vm.runInNewContext(process.argv[2], { event }))
     process.exit(78)
 
 
