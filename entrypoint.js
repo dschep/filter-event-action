28fe15f9b19c670bb87a0efcb709ecf359afd5c8
Daniel Schep <dschep@gmail.com>
2019-07-11 14:06:08 -0400

    add debug

--- entrypoint.js
+++ entrypoint.js
@@ -5,6 +5,10 @@ const vm = require('vm')
 
 const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH))
 
+if (process.env.DEBUG) {
+    console.log(event)
+}
+
 if (!vm.runInNewContext(process.argv[2], { event }))
     process.exit(78)
 
